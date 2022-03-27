6c73916f15c07e332cd125c2b175d0d28af0cf80
Kozimov <kozimovziyoxiddin1415@gmail.com>
2022-03-27 12:53:24 +0500

    Create Meta class in ChartJS

--- chart/settings.py
+++ chart/settings.py
@@ -75,7 +75,7 @@ DATABASES = {
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
 
-AUTH_PASSWORD_VALIDATORS = [
+AUTH_PASSWORD_VALIDATORS = [ 
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
     },
