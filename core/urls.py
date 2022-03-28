cdebe87ba83c81cd7ddbd3d4764ab42c279d5956
Kozimov <kozimovziyoxiddin1415@gmail.com>
2022-03-28 11:01:24 +0500

    post see in html file

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,8 @@
 
 from django.urls import path
-from .views import home
+from .views import *
 
 urlpatterns = [
-    path('', home)
+    path('', home),
+    path('example/', example),
 ]
--- core/views.py
+++ core/views.py
@@ -1,4 +1,13 @@
+from multiprocessing import context
 from django.shortcuts import render
+from .models import Post
 
 def home(request):
     return render(request, 'index.html')
+
+def example(request):
+    posts = Post.objects.all()
+    context = {
+        "posts": posts
+    }
+    return render(request, 'example.html', context)
